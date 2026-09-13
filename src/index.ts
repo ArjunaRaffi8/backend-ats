83ab33a82b94df8185207c2aee7a45e72174890c
Arjuna Raffi Sofyan Pratama <raffitamaa8@gmail.com>
2026-09-13 14:52:20 +0700

    create categoty

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import postRouter from "./routes/posts/posts.routes";
+import categoryRouter from "./routes/category/category.routes";
 
 const app = express();
 const PORT = 3000;
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/category", categoryRouter);
 
 app.get("/", (req, res) => {
     res.send("Hello Express + TypeScript!");
